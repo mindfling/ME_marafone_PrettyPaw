98c61123e31350722c1c56c26f573e4549e2fcf0
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-05-07 17:29:57 +0300

    feat fix video with js by Maks

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ import './index.html';
 //
 import './index.scss';
 
+
+// * use it for video in .hero
+const videobg = document.querySelector('.hero__video-bg');
+
+videobg.innerHTML = `
+  <source src="video/video.webm" type="video/webm">
+  <source src="video/video.mp4" type="video/mp4">
+`;
